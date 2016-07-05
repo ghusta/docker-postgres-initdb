07c2b302877810cd33942af1cd6250a4f203ac5c
ghusta <guillaume.husta@douane.finances.gouv.fr>
2016-07-05 17:31:25 +0200

    Initial commit

---
+++ initdb/initdb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE USER test;
+    CREATE DATABASE test;
+    GRANT ALL PRIVILEGES ON DATABASE test TO test;
+	
+	CREATE TABLE person
+	(
+		id 		serial PRIMARY KEY,
+		name	varchar(50) not null,
+		age		integer
+	);
+EOSQL
